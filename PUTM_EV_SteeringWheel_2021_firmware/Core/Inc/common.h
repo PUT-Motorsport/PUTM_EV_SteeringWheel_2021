4f9b8987bb0ec2f0ec86d1c494f23a7f88756dee
Piotr Kwiatkowski <kwiat1998PP@gmail.com>
2021-05-29 23:54:30 +0200

    Added full functionality

---
+++ PUTM_EV_SteeringWheel_2021_firmware/Core/Inc/common.h
@@ -0,0 +1,19 @@
+//
+// Created by kwitnoncy on 29.05.2021.
+//
+
+#ifndef PUTM_EV_STEERINGWHEEL_2021_FIRMWARE_COMMON_H
+#define PUTM_EV_STEERINGWHEEL_2021_FIRMWARE_COMMON_H
+
+#include "stdint-gcc.h"
+
+#define STEERING_WHEEL_ID   0x00
+#define STEERING_WHEEL_DLC  0x00
+
+#define BUTTON_OFFSET_1     0x00
+#define BUTTON_OFFSET_2     0x00
+
+
+void prepareAnalogSwitchData();
+
+#endif //PUTM_EV_STEERINGWHEEL_2021_FIRMWARE_COMMON_H
